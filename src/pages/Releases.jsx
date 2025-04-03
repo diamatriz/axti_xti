9a7829bd2f13e4a83965cdba54a8ee1129b5705e
diamatriz <diamatrix@yandex.ru>
2025-04-04 00:34:49 +0500

    Обновление: добавлены медиа-запросы для 4K и другие улучшения

--- src/pages/Releases.jsx
+++ src/pages/Releases.jsx
@@ -1,17 +1,14 @@
-// src/pages/Releases.jsx
 import React, { useState } from 'react';
 import '../styles/releases.css'; // Стили для раздела "Релизы"
 
 const Releases = () => {
-  const [activeRelease, setActiveRelease] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const releases = [
     {
       title: 'axti-xti (2020)',
       iframe: (
         <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
           src="https://bandcamp.com/EmbeddedPlayer/album=4051840592/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
         ></iframe>
       ),
     },
@@ -19,17 +16,9 @@ const Releases = () => {
       title: 'кастор/шамиль (2020)',
       iframe: (
         <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
           src="https://bandcamp.com/EmbeddedPlayer/album=1352573240/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
-        ></iframe>
-      ),
-    },
-    {
-      title: 'город пермь (2021)',
-      iframe: (
-        <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
-          src="https://bandcamp.com/EmbeddedPlayer/album=2870844002/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
+          tracklist
         ></iframe>
       ),
     },
@@ -37,8 +26,44 @@ const Releases = () => {
       title: 'знак налицо (2021)',
       iframe: (
         <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
           src="https://bandcamp.com/EmbeddedPlayer/album=2724301580/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
+        ></iframe>
+      ),
+    },
+    {
+      title: 'сласти mix (2021)',
+      iframe: (
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=2518820979/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
+        ></iframe>
+      ),
+    },
+    {
+      title: 'да (2021)',
+      iframe: (
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/track=2322998066/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/transparent=true/"
+          allow="autoplay"
+        ></iframe>
+      ),
+    },
+    {
+      title: 'город пермь (2021)',
+      iframe: (
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=2870844002/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
+        ></iframe>
+      ),
+    },
+    {
+      title: 'музыка для танцев (re-mixed) (2021)',
+      iframe: (
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=3905639076/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
         ></iframe>
       ),
     },
@@ -46,17 +71,8 @@ const Releases = () => {
       title: 'вселенский собор (2022)',
       iframe: (
         <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
           src="https://bandcamp.com/EmbeddedPlayer/album=3841542184/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
-        ></iframe>
-      ),
-    },
-    {
-      title: 'вериги feat. узница совести (2023)',
-      iframe: (
-        <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
-          src="https://bandcamp.com/EmbeddedPlayer/album=3822946170/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/"
+          allow="autoplay"
         ></iframe>
       ),
     },
@@ -64,8 +80,44 @@ const Releases = () => {
       title: 'попробуй чем-нибудь сняться (2023)',
       iframe: (
         <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
           src="https://bandcamp.com/EmbeddedPlayer/album=1739533838/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
+        ></iframe>
+      ),
+    },
+    {
+      title: 'вериги feat. узница совести (2023)',
+      iframe: (
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=3822946170/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/transparent=true/"
+          allow="autoplay"
+        ></iframe>
+      ),
+    },
+    {
+      title: 'uno style (axti-xti remix)(2024)',
+      iframe: (
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=1252795117/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
+        ></iframe>
+      ),
+    },
+    {
+      title: 'вживую @ обычная галерея (2024)',
+      iframe: (
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=673295955/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
+        ></iframe>
+      ),
+    },
+    {
+      title: 'основы ножевого боя remixes (2025)',
+      iframe: (
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=2815117716/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
         ></iframe>
       ),
     },
@@ -73,21 +125,39 @@ const Releases = () => {
       title: 'аптека (2025)СВЕЖАК!!!',
       iframe: (
         <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
           src="https://bandcamp.com/EmbeddedPlayer/album=2875574963/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
         ></iframe>
       ),
     },
     {
-      title: 'секс грибница (2025)СВЕЖАК!!!' ,
+      title: 'секс грибница (2025)СВЕЖАК!!!',
       iframe: (
         <iframe
-          style={{ border: 0, width: '100%', height: '500px' }}
           src="https://bandcamp.com/EmbeddedPlayer/album=685147066/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          allow="autoplay"
         ></iframe>
       ),
     },
   ];
+
+  // Находим индекс релиза "секс грибница"
+  const defaultReleaseIndex = releases.findIndex(
+    (release) => release.title === 'секс грибница (2025)СВЕЖАК!!!'
+  );
+
+  // Устанавливаем начальное состояние activeRelease
+  const [activeRelease, setActiveRelease] = useState(defaultReleaseIndex);
+
+  // Функция для промотки до плеера с задержкой
+  const scrollToPlayer = () => {
+    setTimeout(() => {
+      const playerElement = document.getElementById('release-player');
+      if (playerElement) {
+        playerElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 1000); // Задержка 1000 мс (1 секунды)
+  };
 
   return (
     <section className="releases-section">
@@ -97,7 +167,10 @@ const Releases = () => {
           <div
             key={index}
             className={`release-link ${activeRelease === index ? 'active' : ''}`}
-            onClick={() => setActiveRelease(index)}
+            onClick={() => {
+              setActiveRelease(index); // Устанавливаем активный релиз
+              scrollToPlayer(); // Проматываем до плеера с задержкой
+            }}
           >
             {release.title}
           </div>
@@ -105,7 +178,7 @@ const Releases = () => {
       </div>
 
       {/* Правая колонка с iframe */}
-      <div className="release-player">
+      <div id="release-player" className="release-player">
         {activeRelease !== null && releases[activeRelease].iframe}
       </div>
     </section>
